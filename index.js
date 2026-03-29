aa635f705aa4769e5505d6e6c0cd2adc4e67da69
Balancedenge <yasuomekanik@gmail.com>
2026-03-29 08:50:16 +0300

    index.js

--- index.js
+++ index.js
@@ -3,17 +3,14 @@ const fetch = require("node-fetch");
 const app = express();
 
 const API_KEY = process.env.RIOT_API_KEY;
-const GAME_NAME = "Balanced"; // Riot ID’in gameName kısmı
-const TAG_LINE = "TR1";       // Riot ID’in tagLine kısmı
+const NAME = process.env.SUMMONER_NAME;
 
 app.get("/", async (req, res) => {
   try {
-    // TR server kullanıyoruz
     const summoner = await fetch(
-      `https://tr1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${GAME_NAME}?api_key=${API_KEY}`
+      `https://tr1.api.riotgames.com/lol/summoner/v4/summoners/by-name/${NAME}?api_key=${API_KEY}`
     ).then(r => r.json());
 
-    // Ranked bilgisi çek
     const ranked = await fetch(
       `https://tr1.api.riotgames.com/lol/league/v4/entries/by-summoner/${summoner.id}?api_key=${API_KEY}`
     ).then(r => r.json());
@@ -23,10 +20,9 @@ app.get("/", async (req, res) => {
     if (!solo) return res.send("Unranked");
 
     res.send(`${solo.tier} ${solo.rank} - ${solo.leaguePoints} LP`);
-  } catch (err) {
-    console.log(err); // Hata detayını görmek için
+  } catch {
     res.send("Hata");
   }
 });
 
-app.listen(3000, () => console.log("Server çalışıyor"));
+app.listen(3000);
